85d5a9746d4f38ff59d136c31deda8a433c4b8bf
gregverdov <110197589+gregverdov@users.noreply.github.com>
2022-07-29 17:11:41 -0400

    Add files via upload

---
+++ Rock,Paper,Scissors.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import random 
+
+user_wins = 0
+computer_wins = 0
+choices  = ["rock","paper","scissors"]
+
+while True:
+    user_input = input("Welcome to Rock, Paper, Scissors! Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == 'q':
+        break
+        
+    if user_input not in choices:
+        print("Please select a valid choice!")
+        continue 
+        
+    random_number = random.randint(0, 2)
+    # rock: 0, paper: 1, scissors: 2
+    computer_guess = choices[random_number]
+    print("Computer chooses", computer_guess + "!")
+    
+    if user_input == "rock" and computer_guess == "scissors":
+        print("YOU WIN! YOU GET A POINT")
+        user_wins += 1 
+        
+    elif user_input == "paper" and computer_guess == "rock":
+        print("YOU WIN! YOU GET A POINT")
+        user_wins += 1
+        
+    elif user_input == "scissors" and computer_guess == "paper":
+        print("YOU WIN! YOU GET A POINT")
+        user_wins += 1
+    
+    else:
+        print("YOU LOST! COMPUTER GETS A POINT!")
+        computer_wins += 1
+        
+print("You won", user_wins, "times.")
+print("computer has won", computer_wins, "times.")
+print("Goodbye!")
+
+
+# In[ ]:
+
+
+
+
